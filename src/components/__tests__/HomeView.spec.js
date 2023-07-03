df2a1e3cdadb88f736f77a7d98b84e833054688b
Daniel Crespo <dani.crespo.rodriguez@gmail.com>
2023-07-03 20:23:04 +0200

    3 test

--- src/components/__tests__/HomeView.spec.js
+++ src/components/__tests__/HomeView.spec.js
@@ -1,30 +1,60 @@
 import { describe, it, expect } from 'vitest'
-
 import { mount } from '@vue/test-utils'
 import HomeView from '../../views/HomeView.vue'
 
 describe('HomeView', () => {
-
   //Defino donde busco
-  const wrapper = mount(HomeView);
-  
-  it('has 9 input labels', () => {    
-    
+  const wrapper = mount(HomeView)
+
+  it('has 9 input labels', () => {
     //Busco los hijos directos que sean input
-    let numOfInputs=wrapper.find("[id='zonaJuego']").element.querySelectorAll(":scope > input").length;
-    
+    let numOfInputs = wrapper.find("[id='zonaJuego']").element.querySelectorAll(':scope > input').length
+
     //Testeo si esta bien
-    expect(numOfInputs).toBe(9);
+    expect(numOfInputs).toBe(9)
   })
 
-  it('each input is clickable', () =>{
-    let arrayOfInputs=wrapper.find("[id='zonaJuego']").element.querySelectorAll(":scope > input");
-    
-    arrayOfInputs.forEach(eachInput=>{
+  it('has bottons that change their value when clicked', () => {
+    let arrayOfInputs = wrapper.find("[id='zonaJuego']").element.querySelectorAll(':scope > input');
 
-      expect(eachInput).toBe()
+    let arrayOfValuesBefore=[];
+    let arrayOfValuesAfter=[];
 
+    arrayOfInputs.forEach((eachInput) => {  
+      
+      arrayOfValuesBefore.push(eachInput.getAttribute("value"));
+
+      eachInput.dispatchEvent(new Event("click"));
+
+      arrayOfValuesAfter.push(eachInput.getAttribute("value")); 
     })
 
+    //console.log(arrayOfValuesBefore);
+    //console.log(arrayOfValuesAfter);
+
+    expect(arrayOfValuesBefore===arrayOfValuesAfter).toBe(false);
   })
+
+  it('does not change the value of a previously changed grid square', () => {
+    let arrayOfInputs = wrapper.find("[id='zonaJuego']").element.querySelectorAll(':scope > input');
+
+    let arrayOfValuesBefore=[];
+    let arrayOfValuesAfter=[];
+
+    arrayOfInputs.forEach((eachInput) => {  
+      
+      arrayOfValuesBefore.push(eachInput.getAttribute("value"));
+
+      eachInput.dispatchEvent(new Event("click"));
+
+      arrayOfValuesAfter.push(eachInput.getAttribute("value")); 
+    })
+
+    console.log(arrayOfValuesBefore);
+    console.log(arrayOfValuesAfter);
+    
+    expect(arrayOfValuesBefore).toStrictEqual(arrayOfValuesAfter); 
+  })
+
+
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,84 @@
 <script setup>
 
-let casilla_1_1;
-let casilla_1_2;
-let casilla_1_3;
-let casilla_2_1;
-let casilla_2_2;
-let casilla_2_3;
-let casilla_3_1;
-let casilla_3_2;
-let casilla_3_3;
+import { onMounted, ref } from 'vue'
 
-let tablero=[ [casilla_1_1, casilla_1_2, casilla_1_3],
-              [casilla_2_1, casilla_2_2, casilla_2_3],
-              [casilla_3_1, casilla_3_2, casilla_3_3]];         
+let casilla_1_1
+let casilla_1_2
+let casilla_1_3
+let casilla_2_1
+let casilla_2_2
+let casilla_2_3
+let casilla_3_1
+let casilla_3_2
+let casilla_3_3
+
+const playerNum=ref(1);
+
+const misPruebas = ref();
+
+let tablero = [
+  [casilla_1_1, casilla_1_2, casilla_1_3],
+  [casilla_2_1, casilla_2_2, casilla_2_3],
+  [casilla_3_1, casilla_3_2, casilla_3_3]
+];
+
+
+/*
+function AddClickListener(){
+  let arrayInputs= document.querySelectorAll("#zonaJuego input");
+
+  arrayInputs.forEach((eachInput, index)=>{
+    eachInput.addEventListener("click", (event) =>{
+      //console.log("hola");
+      misPruebas.value="PasoPorAqui" + event.target.value;
+    });    
+  })
+
+}
+
+
+onMounted(() => {
+  let esto=AddClickListener();
+  console.log(esto);
+  
+})*/
+
+function changeValue(event){
+
+  let myTarget=event.target;
+  
+  if(myTarget.getAttribute("value")===null){
+
+    myTarget.setAttribute("value", TranslateIntoSimbol());
+    
+    playerNum.value=ChangePlayer();
+    
+  }
+  else{
+    alert("Casilla ocupada");
+  }
+ 
+
+}
+
+function TranslateIntoSimbol(){
+  if(playerNum.value===1){
+    return "X";
+  }
+  if(playerNum.value===2){
+    return "O"
+  }
+}
+
+function ChangePlayer(){
+  if(playerNum.value===1){
+    return 2;
+  }
+  if(playerNum.value===2){
+    return 1;
+  }
+}
+
 
 
 </script>
@@ -20,38 +86,38 @@ let tablero=[ [casilla_1_1, casilla_1_2, casilla_1_3],
 <template>
   <main>
     <div>
-      <label>Turno del Jugador: <scope>1</scope></label>
+      <label>Turno del Jugador: <scope>{{playerNum}}</scope></label>
+      <p>{{ misPruebas }}</p>
     </div>
-    
-    <div id="zonaJuego">
-    <input maxlength="1" type="Button" value="">
-    <input maxlength="1" type="Button" value="">
-    <input maxlength="1" type="Button" value="">
-    <input maxlength="1" type="Button" value="">
-    <input maxlength="1" type="Button" value="">
-    <input maxlength="1" type="Button" value="">
-    <input maxlength="1" type="Button" value="">
-    <input maxlength="1" type="Button" value="">
-    <input maxlength="1" type="Button" value="">
 
-    </div>     
-    
+    <div id="zonaJuego">
+      
+      <input maxlength="1" type="Button" value="" tag="1" @click="changeValue($event)"/>
+      <input maxlength="1" type="Button" value="" tag="2" @click="changeValue($event)"/>
+      <input maxlength="1" type="Button" value="" tag="3" @click="changeValue($event)"/>
+      <input maxlength="1" type="Button" value="" tag="4" @click="changeValue($event)"/>
+      <input maxlength="1" type="Button" value="" tag="5" @click="changeValue($event)"/>
+      <input maxlength="1" type="Button" value="" tag="6" @click="changeValue($event)"/>
+      <input maxlength="1" type="Button" value="" tag="7" @click="changeValue($event)"/>
+      <input maxlength="1" type="Button" value="" tag="8" @click="changeValue($event)"/>
+      <input maxlength="1" type="Button" value="" tag="9" @click="changeValue($event)"/>
+    </div>
   </main>
 </template>
 
 <style scoped>
-div{
+div {
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
   align-items: center;
 }
 
-:deep()input{
-  border:1px solid black;
-  width:30%;
+:deep()input {
+  border: 1px solid black;
+  width: 30%;
   aspect-ratio: 1/1;
-  size: 1; 
+  size: 1;
   text-align: center;
   font-size: 15vw;
 }
